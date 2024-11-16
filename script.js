e5e19786e3cdfd886689efccf19535bac744a285
Vova228_51 <tank.vovi.pavlova@gmail.com>
2024-11-16 16:31:51 +0300

    print clock for site

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function updateClock(){
-
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 }
-
-document.getElementById('clock').textContent = new Date();
+setInterval(updateClock, 1000);
