4b1dccfcf6ba725327df1892cbca3065a97db99a
Vova228_51 <tank.vovi.pavlova@gmail.com>
2024-11-16 17:40:55 +0300

    Add UTC Time

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function updateClock() {
+function updateClock(time) {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, "0");
-    const minutes = String(now.getMinutes()).padStart(2, "0");
-    const seconds = String(now.getSeconds()).padStart(2, "0");
+    const hours = String(now.getUTCHours() + time).padStart(2, "0");
+    const minutes = String(now.getUTCMinutes()).padStart(2, "0");
+    const seconds = String(now.getUTCSeconds()).padStart(2, "0");
+    selectTimeZona.addEventListener('change', updateClock(timeZona));
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+    const selectTimeZona = document.getElementById('timeZona'); 
+    const timeZona = parseInt(selectTimeZona.value, 10);
 }
-setInterval(updateClock, 1000);
+
+
+// console.log(timeZona)
+
+
+setInterval(updateClock(timeZona), 1000);
+
+
+
+// const difrent_time_UTC = new Date().getTimezoneOffset() / 60;
+// const timeZona = Intl.DateTimeFormat().resolvedOptions().timeZona;
+// const timeZona2 = new Date().DateTimeFormat().resolvedOptions().timeZona;
+// document.getElementById('clock2').textContent = timeZona;
