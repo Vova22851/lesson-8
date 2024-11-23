4b4f94890762a89c94598ec423165a9f5b1423d8
Vova228_51 <tank.vovi.pavlova@gmail.com>
2024-11-23 17:26:39 +0300

    New style, enter UTC time with select country

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-function updateClock(time) {
+const clockElement = document.getElementById('clock');
+const selectTimeZona = document.getElementById('timeZona');
+const local_timezona = document.getElementById('local')
+
+function updateClock() {
     const now = new Date();
-    const hours = String(now.getUTCHours() + time).padStart(2, "0");
-    const minutes = String(now.getUTCMinutes()).padStart(2, "0");
-    const seconds = String(now.getUTCSeconds()).padStart(2, "0");
-    selectTimeZona.addEventListener('change', updateClock(timeZona));
-    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
-    const selectTimeZona = document.getElementById('timeZona'); 
     const timeZona = parseInt(selectTimeZona.value, 10);
+    const hours = String(now.getUTCHours() + timeZona).padStart(2, "0");
+    const minutes = String(now.getUTCMinutes()).padStart(2, "0");;
+    const seconds = String(now.getUTCSeconds()).padStart(2, "0");
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+    local_timezona.textContent = Intl.DateTimeFormat().resolvedOptions().timeZone;
 }
 
+// let timeZona = 0;
 
-// console.log(timeZona)
+//console.log(timeZona);
 
+setInterval(updateClock, 1000);
 
-setInterval(updateClock(timeZona), 1000);
-
+selectTimeZona.addEventListener('change', updateClock(timeZona));
 
 
 // const difrent_time_UTC = new Date().getTimezoneOffset() / 60;
-// const timeZona = Intl.DateTimeFormat().resolvedOptions().timeZona;
-// const timeZona2 = new Date().DateTimeFormat().resolvedOptions().timeZona;
-// document.getElementById('clock2').textContent = timeZona;
+// const timeZona = Intl.DateTimeFormat().resolvedOptions().timeZone;
+// const timeZona2 = new Date().DateTimeFormat().resolvedOptions().timeZone;
+// document.getElementById('clock2').textContent = timeZona2;
